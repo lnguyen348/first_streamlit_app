60b704f8e5a835b3d8a4065434fea34d4d2aff7a
lnguyen348 <97860192+lnguyen348@users.noreply.github.com>
2022-11-10 14:49:11 -0800

    Update streamlit_app.py
    fixed code line error

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,9 +58,6 @@ if streamlit.button('Get Fruit Load List'):
     streamlit.dataframe(my_data_rows)
 
 
-streamlit.dataframe(my_data_rows)
-
-
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
